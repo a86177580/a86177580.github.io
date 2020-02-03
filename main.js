5b061f76061e192eed68c6dc88ccbb6982b822e6
a86177580 <60596497+a86177580@users.noreply.github.com>
2020-02-03 15:48:57 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/1.jpg') {
+      myImage.setAttribute ('src','images/2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/1.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Welcome to my blog, ' + myName;
+    
+    if(!localStorage.getItem('name')) {
+      setUserName();
+    } else {
+      var storedName = localStorage.getItem('name');
+      myHeading.innerHTML = 'Welcome to my blog, ' + storedName;
+    }
+    
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
